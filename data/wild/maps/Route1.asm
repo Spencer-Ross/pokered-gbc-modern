d4df2e6ab5ef074097bc1afacf5b1b7f2493b187
Spencer <spencer.ross@wsu.edu>
2023-09-16 23:25:46 -0700

    added starters to route 1

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -1,15 +1,15 @@
 Route1WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, BULBASAUR
-	db  3, CHARMANDER
-	db  3, SQUIRTLE
-	db  2, RATTATA
-	db  2, PIDGEY
-	db  3, EEVEE
-	db  3, DRATINI
-	db  4, RATTATA
-	db  4, EEVEE
-	db  5, PIDGEY
+	db  3, PIDGEY		; 20%
+	db  3, RATTATA		; 20%
+	db  3, EEVEE		; 15%
+	db  3, BULBASAUR	; 10%
+	db  3, SQUIRTLE		; 10%
+	db  3, CHARMANDER	; 10%
+	db  3, PIDGEY		; 5%
+	db  4, RATTATA		; 5%
+	db  4, DRATINI		; 4%
+	db  5, SPEAROW		; 1%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
